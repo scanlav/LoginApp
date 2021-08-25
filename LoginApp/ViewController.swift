ea321b67660d7933db057301baa2c8b0bcd735c9
Viktor Churikov <scanlave@gmail.com>
2021-08-26 02:48:22 +0800

    Constraint, code, design

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -9,11 +9,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var logInButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    @IBOutlet var forgotLoginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func logInButtonPressed() {
+        if loginTextField.text == "Admin",
+           passwordTextField.text == "Admin111" {
+            
+        } else {
+            let okButtonAction = UIAlertAction(
+                title: "OK",
+                style: .cancel
+            ) { action in
+                self.passwordTextField.text = ""
+            }
+            let alert = UIAlertController(
+                title: "Access error",
+                message: "You entered an incorrect username or password",
+                preferredStyle: .alert
+            )
+            alert.addAction(okButtonAction)
+            self.present(alert, animated: true)
+        }
+    }
+    
+    @IBAction func forgotLoginPressed() {
+        let okButtonAction = UIAlertAction(
+            title: "OK",
+            style: .cancel
+        )
+        let alert = UIAlertController(
+            title: "Forgot login",
+            message: "Your login Admin",
+            preferredStyle: .alert
+        )
+        alert.addAction(okButtonAction)
+        self.present(alert, animated: true)
+    }
+    
+    @IBAction func forgotPasswordPressed() {
+        let okButtonAction = UIAlertAction(
+            title: "OK",
+            style: .cancel
+        ) { action in
+            self.passwordTextField.text = ""
+        }
+        let alert = UIAlertController(
+            title: "Forgot password",
+            message: "Your password Admin111",
+            preferredStyle: .alert
+        )
+        alert.addAction(okButtonAction)
+        self.present(alert, animated: true)
+    }
+    
 }
 
